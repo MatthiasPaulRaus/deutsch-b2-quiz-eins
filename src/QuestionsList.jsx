c6f3b5fd7c10977bd8caa6baaa0706a1f8f17179
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-05-24 18:53:50 +0200

    first commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Wir haben nicht nur regelmäßig miteinander telefoniert,",
+        answersList: [
+            {answer:"sondern uns auch oft getroffen.", isCorrect:true},
+            {answer:"sondern wir uns auch gesehen.", isCorrect:false},
+            {answer:"sondern auch oft uns gemailt.", isCorrect:false},
+        ]
+    },{
+        question:"Es macht weder meiner Freundin",
+        answersList: [
+            {answer:"oder mir etwas aus.", isCorrect:false},
+            {answer:"noch mir etwas aus.", isCorrect:true},
+            {answer:"und mir auch nichts aus.", isCorrect:false},
+        ]
+    },{
+        question:"Entweder gehen wir einkaufen",
+        answersList: [
+            {answer:"und wir bleiben zuhause.", isCorrect:false},
+            {answer:"oder bleiben nicht zuhause.", isCorrect:false},
+            {answer:"oder wir bleiben zuhause.", isCorrect:true},
+        ]
+    },{
+        question:"Du kannst entweder zu mir kommen",
+        answersList: [
+            {answer:"oder wir treffen uns in der Stadt.", isCorrect:true},
+            {answer:"und wir treffen uns dann dort.", isCorrect:false},
+            {answer:"oder aber ich kann nicht kommen.", isCorrect:false},
+        ]
+    },{
+        question:"Einerseits würden wir gerne reisen,",
+        answersList: [
+            {answer:"auf der anderen Seite ist es auch sehr schön.", isCorrect:false},
+            {answer:"andererseits ist es hier auch sehr schön.", isCorrect:true},
+            {answer:"andere Länder sind für uns sehr anziehend.", isCorrect:false},
+        ]
+    },{
+        question:"Ich sehe sie zwar sehr selten,",
+        answersList: [
+            {answer:"aber ich erinnere mich nicht mehr an sie.", isCorrect:false},
+            {answer:"aber sie bleibt oft bei ihren Eltern zuhause.", isCorrect:false},
+            {answer:"aber sie bleibt mir in guter Erinnerung.", isCorrect:true},
+        ]
+    },{
+        question:"Die Firma machte schon früher",
+        answersList: [
+            {answer:"mit freiwilligen Studenten aus Europa Untersuchungen.", isCorrect:true},
+            {answer:"Experimente über freiwillige Teilnehmer in Asien.", isCorrect:false},
+            {answer:"mit Forschungen an Freiwilligen und Studenten.", isCorrect:false},
+        ]
+    },{
+        question:"Wir sind heutzutage",
+        answersList: [
+            {answer:"wirklich verbunden weltweit wegen den Netzwerken.", isCorrect:false},
+            {answer:"wegen der sozialen Netzwerke weltweit verbunden.", isCorrect:true},
+            {answer:"wegen der Vernetzung verbunden weltweit.", isCorrect:false},
+        ]
+    },{
+        question:"Durch diese Übungen",
+        answersList: [
+            {answer:"können Sie schnell Stress bauen.", isCorrect:false},
+            {answer:"kann sich Ihr Stress abgebaut werden.", isCorrect:false},
+            {answer:"kann Ihr Stress schnell abgebaut werden.", isCorrect:true},
+        ]
+    },{
+        question:"Das Seminar wird von einem",
+        answersList: [
+            {answer:"erfahrenen Trainer durchgeführt.", isCorrect:true},
+            {answer:"Trainer mit Erfahrung gefahren.", isCorrect:false},
+            {answer:"Spezialisten gefühlt.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Das Ausbildungsprogramm wird",
+        answersList: [
+            {answer:"von staatlicher Seite finanziert.", isCorrect:true},
+            {answer:"mit dem Finanzamt unterstützt.", isCorrect:false},
+            {answer:"staatlich vom Finanzamt hergestellt.", isCorrect:false},
+        ]
+    },{
+        question:"Die Informationen werden",
+        answersList: [
+            {answer:"über den Leitenden verhandelt.", isCorrect:false},
+            {answer:"vom Projektleiter recherchiert.", isCorrect:true},
+            {answer:"durch Auszubildende durchgeführt.", isCorrect:false},
+        ]
+    },{
+        question:"Die Unterlagen für Ihre Bewerbung",
+        answersList: [
+            {answer:"sind auf der website zu recherchieren.", isCorrect:false},
+            {answer:"müssen sie online organisieren.", isCorrect:false},
+            {answer:"können auf unserer homepage gefunden werden.", isCorrect:true},
+        ]
+    },{
+        question:"Bewerbende bei unserer Firma sollten",
+        answersList: [
+            {answer:"die neuesten Computerprogramme kennen.", isCorrect:true},
+            {answer:"gemeinsame Freizeitaktivitäten haben.", isCorrect:false},
+            {answer:"mehrere Fremdsprachen organisieren.", isCorrect:false},
+        ]
+    },{
+        question:"Aufgrund Ihres langen Weges zur Arbeit",
+        answersList: [
+            {answer:"haben Sie keine Zeit für Pausen.", isCorrect:false},
+            {answer:"verbringen Sie viel Zeit im Zug.", isCorrect:true},
+            {answer:"sind Sie in viele Länder gereist.", isCorrect:false},
+        ]
+    },{
+        question:"Aus Höflichkeit sollten Sie",
+        answersList: [
+            {answer:"einem Passagier schnell ein Angebot machen.", isCorrect:false},
+            {answer:"den Passagieren ihre Plätze anweisen.", isCorrect:false},
+            {answer:"Ihren Platz den älteren Passagieren anbieten.", isCorrect:true},
+        ]
+    },{
+        question:"Es war keinesfalls meine Absicht,",
+        answersList: [
+            {answer:"mich bei dieser Firma zu bewerben.", isCorrect:true},
+            {answer:"einen Bewerber bei der Firma zu bestätigen.", isCorrect:false},
+            {answer:"eine Bewerbung bei der Firma anzubieten.", isCorrect:false},
+        ]
+    },{
+        question:"Dank Ihrer Hinweise",
+        answersList: [
+            {answer:"konnten Sie uns missverstehen.", isCorrect:false},
+            {answer:"konnten wir das Missverständnis klären.", isCorrect:true},
+            {answer:"hatte ich Sie missverstanden.", isCorrect:false},
+        ]
+    },{
+        question:"Es ist mir wirklich unangehehm,",
+        answersList: [
+            {answer:"es Ihnen zu bescheiden, weil sie nicht grüßen.", isCorrect:false},
+            {answer:"obwohl ich Sie nicht gegrüßt habe.", isCorrect:false},
+            {answer:"dass ich Ihnen nicht Bescheid gesagt habe.", isCorrect:true},
+        ]
+    },{
+        question:"Vor Müdigkeit",
+        answersList: [
+            {answer:"konnte ich keine klaren Gedanken fassen.", isCorrect:true},
+            {answer:"hätte ich es klar verstehen müssen.", isCorrect:false},
+            {answer:"wäre ich an deiner Stelle sofort aufgewacht.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Wahrscheinlich ist es problematisch,",
+        answersList: [
+            {answer:"wenn Sie ohne Vertrag arbeiten sollen.", isCorrect:true},
+            {answer:"wenn Sie einen Vertrag unterzeichnen.", isCorrect:false},
+            {answer:"wenn der Vertrag erfüllt werden muss.", isCorrect:false},
+        ]
+    },{
+        question:"Es kann ein Grund für eine Kündigung sein,",
+        answersList: [
+            {answer:"die Kaffeepause ausfallen zu lassen.", isCorrect:false},
+            {answer:"Fotokopien für Ihren Privatgebrauch zu machen.", isCorrect:true},
+            {answer:"Ihre privaten Kopien dafür zu gebrauchen.", isCorrect:false},
+        ]
+    },{
+        question:"Diese Sozialform ermöglicht es",
+        answersList: [
+            {answer:"alle Mitarbeiter an der Entscheidung teilzunehmen", isCorrect:false},
+            {answer:"dass alle Mitarbeiter alle Entscheidungen treffen.", isCorrect:false},
+            {answer:"alle Mitarbeiter teilnehmen zu lassen.", isCorrect:true},
+        ]
+    },{
+        question:"Die bestehenden Probleme sind",
+        answersList: [
+            {answer:"vermutlich einfach zu lösen.", isCorrect:true},
+            {answer:"wahrscheinlich eine einfache Lösung.", isCorrect:false},
+            {answer:"höchstwahrscheinlich keine einfache Lösung.", isCorrect:false},
+        ]
+    },{
+        question:"Welche Medien benutzen Sie in Ihrer Freizeit?",
+        answersList: [
+            {answer:"Am liebsten lese ich meinen Kindern Geschichten vor.", isCorrect:false},
+            {answer:"Printmedien lese ich immer weniger.", isCorrect:false},
+            {answer:"Ich lese hauptsächlich online-Zeitungen und höre podcasts.", isCorrect:true},
+        ]
+    },{
+        question:"Die Daten deuten darauf hin,",
+        answersList: [
+            {answer:"dass der Medienkonsum stark zugenommen hat.", isCorrect:true},
+            {answer:"dass sich die Medien nicht gewandelt haben.", isCorrect:false},
+            {answer:"dass wir die Medien nicht verändern sollten.", isCorrect:false},
+        ]
+    },{
+        question:"Ich kann mir heutzutage gar nicht mehr vorstellen,",
+        answersList: [
+            {answer:"dass ich ohne labtop gehe.", isCorrect:false},
+            {answer:"ohne mein Handy aus dem Haus zu gehen.", isCorrect:true},
+            {answer:"dass er noch ein Handy benutzt.", isCorrect:false},
+        ]
+    },{
+        question:"Bemerkenswert ist der Erfolg der Sängerin",
+        answersList: [
+            {answer:"sowohl bei den jüngsten Sängern als bei alten.", isCorrect:false},
+            {answer:"beim Singen ohne das ältere Publikum.", isCorrect:false},
+            {answer:"sowohl bei jüngeren als auch bei älteren Leuten.", isCorrect:true},
+        ]
+    },{
+        question:"Sind keine Eintrittskarten mehr zu bekommen,",
+        answersList: [
+            {answer:"können wir es online versuchen.", isCorrect:true},
+            {answer:"dann sollten wir es mit Karten versuchen.", isCorrect:false},
+            {answer:"haben wir es an der Kasse versucht.", isCorrect:false},
+        ]
+    },{
+        question:"Die Leiterin kommt später,",
+        answersList: [
+            {answer:"wenn sie morgen keine Verspätung hat.", isCorrect:false},
+            {answer:"obwohl sie meistens Verspätung hat.", isCorrect:false},
+            {answer:"weil ihr Flug heute Verspätung hat.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
